1edcc6425f35d54cc2fdf4d20bcbb1730776a6b9
rokkky <ka.egor1999@gmail.com>
2023-08-30 16:55:28 +0300

    expenses page, reusable table, theme refactor

---
+++ src/app/modules/expenses/models/expenses.interface.ts
@@ -0,0 +1,6 @@
+export interface IExpenses {
+  date: Date;
+  amount: number;
+  description: string;
+  categoryId: number;
+}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FieldErrorComponent } from './components/field-error/field-error.component';
+import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
+import { TableHeaderComponent } from './components/table-header/table-header.component';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [FieldErrorComponent],
-  imports: [CommonModule, MatFormFieldModule],
-  exports: [FieldErrorComponent],
+  declarations: [
+    FieldErrorComponent,
+    TransactionsTableComponent,
+    TableHeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
+  exports: [
+    FieldErrorComponent,
+    TransactionsTableComponent,
+    TableHeaderComponent,
+  ],
 })
 export class SharedModule {}
